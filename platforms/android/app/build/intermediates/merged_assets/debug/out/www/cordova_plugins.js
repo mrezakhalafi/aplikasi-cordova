6178fb45dd79fcdce3fe5bea0cac0b2943b740be
mrezakhalafi <mrezakhalafi@yahoo.com>
2023-04-06 16:00:55 +0700

    First release

---
+++ platforms/android/app/build/intermediates/merged_assets/debug/out/www/cordova_plugins.js
@@ -0,0 +1,48 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-camera.Camera",
+      "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "Camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverOptions",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverOptions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.camera",
+      "file": "plugins/cordova-plugin-camera/www/Camera.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "navigator.camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverHandle",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverHandle"
+      ]
+    },
+    {
+      "id": "cordova-plugin-photos.Photos",
+      "file": "plugins/cordova-plugin-photos/www/Photos.js",
+      "pluginId": "cordova-plugin-photos",
+      "clobbers": [
+        "Photos"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-camera": "6.0.0",
+    "cordova-plugin-photos": "1.0.9"
+  };
+});
